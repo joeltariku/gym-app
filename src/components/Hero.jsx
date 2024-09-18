34fd81ceaf55e0fca96bd352c66bcc239645265e
jtariku <jojotk10@gmail.com>
2024-09-17 22:38:02 -0400

    update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+
+export default function Hero() {
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px]
+    w-full mx-auto p-4">
+        <div className="flex flex-col gap-4">
+        <p>IT'S TIME TO GET</p>
+        <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Swole<span className="text-blue-400">nourmous</span></h1>
+        </div>
+        
+        <p className="text-sm md:text-base font-light">
+            I hearby acknowledgement that I may become unbelievably swolenormous and accept all risks of becoming
+            the local mass montrosity, afflicted with severe body dismorphia, unable to fit through doors.
+        </p>
+        <Button func={() => {
+            window.location.href = "#generate"
+        }} text={"Accept & Begin"}></Button>
+    </div>
+  )
+}
